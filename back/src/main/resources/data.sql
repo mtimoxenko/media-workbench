807900597d6479510aae297d24782eb9807e4cfd
fr3m3n <mtimochenko@tutanota.com>
2024-01-10 19:48:01 -0300

    task name and description to SPANISH [update]

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -10,20 +10,20 @@ VALUES
 -- Insert 5 specific tasks
 INSERT INTO task (name, description, creator_id, creation_date, status)
 VALUES
-    ('Grafana Healthchecks', 'Realizar revisiones regulares de salud y monitoreo de flujos de red utilizando Grafana', 2, '2023-08-01 09:00:00', 'ACTIVE'),
-    ('Traffic Overflow Management with QWILT Filter', 'Manage and optimize traffic overflow using QWILT filtering techniques', 2, '2023-08-02 10:00:00', 'ACTIVE'),
-    ('EDGE Maintenance', 'Perform routine maintenance and updates on the EDGE network infrastructure', 5, '2023-08-03 11:00:00', 'ACTIVE'),
-    ('Pending ICD TTs', 'Resolve pending Trouble Tickets related to issues and ICD queries', 5, '2023-08-04 12:00:00', 'ACTIVE'),
-    ('FMC Alarms', 'Monitor and respond to alarms at the Failure Management Center (FMC)', 2, '2023-08-05 13:00:00', 'ACTIVE');
+    ('Controles de Salud Grafana', 'Realizar revisiones regulares de salud y monitoreo de flujos de red utilizando Grafana', 2, '2023-08-01 09:00:00', 'ACTIVE'),
+    ('Gestión Tráfico con Filtro QWILT', 'Gestionar y optimizar el exceso de tráfico utilizando técnicas de filtrado QWILT', 2, '2023-08-02 10:00:00', 'ACTIVE'),
+    ('Mantenimiento EDGE', 'Realizar mantenimiento de rutina y actualizaciones en la infraestructura de la red EDGE', 5, '2023-08-03 11:00:00', 'ACTIVE'),
+    ('TTs ICD Pendientes', 'Resolver Tickets de Problemas pendientes relacionados con problemas y consultas de ICD', 5, '2023-08-04 12:00:00', 'ACTIVE'),
+    ('Alarmas FMC', 'Monitorear y responder a las alarmas en el Centro de Gestión de Fallas (FMC)', 2, '2023-08-05 13:00:00', 'ACTIVE');
 
 -- Insert 5 random assignments of tasks to users (UserTask)
 INSERT INTO user_task (user_id, task_id, user_task_status, assignment_date, assigned_by)
 VALUES
     (1, 1, 'ASSIGNED', '2023-08-06 14:00:00', 2),
-    (1, 2, 'ASSIGNED', '2023-08-07 15:00:00', 2),
-    (3, 3, 'ASSIGNED', '2023-08-08 09:30:00', 5),
+    (1, 2, 'ASSIGNED', '2023-08-07 15:00:00', 4),
+    (3, 3, 'ASSIGNED', '2023-08-08 09:30:00', 1),
     (4, 4, 'ASSIGNED', '2023-08-09 16:45:00', 5),
-    (5, 5, 'ASSIGNED', '2023-08-10 17:00:00', 2);
+    (5, 5, 'ASSIGNED', '2023-08-10 17:00:00', 3);
 
 -- Insert 5 specific comments
 INSERT INTO comment (user_id, task_id, text, timestamp)
