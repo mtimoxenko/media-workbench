7b2209317376899b94544ec6b539f318bfbd984e
fr3m3n <mtimochenko@tutanota.com>
2024-02-03 01:22:37 -0300

    [++] FULL Schedule API implementation plus @Schema annotations added to the Schedule class

--- back/src/main/java/com/mediaworkbench/workbench/model/Schedule.java
+++ back/src/main/java/com/mediaworkbench/workbench/model/Schedule.java
@@ -1,5 +1,6 @@
 package com.mediaworkbench.workbench.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -17,26 +18,33 @@ public class Schedule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Unique identifier of the Schedule.", example = "100", required = true)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @Schema(description = "The user associated with this schedule.", required = true)
     private User user;
 
     @Column(nullable = false)
+    @Schema(description = "Date of the schedule.", example = "2023-08-15", required = true)
     private LocalDate date;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "day_of_week", nullable = false)
+    @Schema(description = "Day of the week for the schedule.", example = "MONDAY", required = true)
     private DayOfWeek dayOfWeek; // Using java.time.DayOfWeek
 
     @Column(name = "day_number", nullable = false)
+    @Schema(description = "Day number of the schedule.", example = "7", required = true)
     private Integer dayNumber;
 
     @Column(name = "is_working_day", nullable = false)
+    @Schema(description = "Indicates if it's a working day.", example = "true", required = true)
     private Boolean isWorkingDay;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = true)
+    @Schema(description = "Role associated with the schedule.", example = "REPORTER", required = false)
     private UserRoleStatus role;
 }
